3dce4e45e53e5e25ebdd54d88a52f51e656f68d3
Emili <ejanchevis@gmail.com>
2023-11-10 06:40:25 -1000

    homework cart take-2

---
+++ dist/index.js
@@ -0,0 +1,137 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const uuid_1 = require("uuid");
+class Item {
+    constructor(_name, _price, _description, _id = (0, uuid_1.v4)()) {
+        this._name = _name;
+        this._price = _price;
+        this._description = _description;
+        this._id = _id;
+    }
+    get description() {
+        return this._description;
+    }
+    set description(value) {
+        this._description = value;
+    }
+    get price() {
+        return this._price;
+    }
+    set price(value) {
+        this._price = value;
+    }
+    get name() {
+        return this._name;
+    }
+    set name(value) {
+        this._name = value;
+    }
+    get id() {
+        return this._id;
+    }
+    set id(value) {
+        this._id = value;
+    }
+}
+class User {
+    constructor(_name, _age, _cart = [], _id = (0, uuid_1.v4)()) {
+        this._name = _name;
+        this._age = _age;
+        this._cart = _cart;
+        this._id = _id;
+    }
+    get id() {
+        return this._id;
+    }
+    set id(value) {
+        this._id = value;
+    }
+    get cart() {
+        return this._cart;
+    }
+    set cart(value) {
+        this._cart = value;
+    }
+    get age() {
+        return this._age;
+    }
+    set age(value) {
+        this._age = value;
+    }
+    get name() {
+        return this._name;
+    }
+    set name(value) {
+        this._name = value;
+    }
+    addToCart(item) {
+        this.cart.push(item);
+    }
+    removeFromCart(itemToRemove) {
+        this.cart = this.cart.filter(item => item.id !== itemToRemove.id);
+    }
+    removeQuantityFromCart(itemToRemove, quantity) {
+        for (let i = 0; i < quantity; i++) {
+            let index = this.cart.findIndex(item => item.id === itemToRemove.id);
+            this.cart.splice(index, 1);
+        }
+    }
+    getCartTotal() {
+        let total = 0;
+        for (let item of this.cart) {
+            total += item.price;
+        }
+        return total;
+    }
+    printCart() {
+        console.log(`Here is what is in your cart, ${this.name}:`);
+        for (let item of this.cart) {
+            console.log(`${item.name}: $${item.price}`);
+        }
+        console.log(`Total: $${this.getCartTotal()}`);
+    }
+}
+class Shop {
+    constructor(_items = []) {
+        this._items = _items;
+        let itemA = new Item('Notebook', 10, 'Keep all of your notes in one handy place');
+        this.items.push(itemA);
+        let itemB = new Item('Socket Wrench', 20, 'Tools');
+        this.items.push(itemB);
+        let itemC = new Item('Blanket', 30, 'Stay warm and cozy');
+        this.items.push(itemC);
+    }
+    get items() {
+        return this._items;
+    }
+    set items(value) {
+        this._items = value;
+    }
+}
+// Driver Code
+let myShop = new Shop();
+let myUser = new User('Fred', 55);
+// Add 1 itemA to the user
+myUser.addToCart(myShop.items[0]);
+myUser.printCart();
+console.log('=====================================================');
+// Add 3 of itemB to the user
+myUser.addToCart(myShop.items[1]);
+myUser.addToCart(myShop.items[1]);
+myUser.addToCart(myShop.items[1]);
+myUser.printCart();
+console.log('=====================================================');
+// Add 4 of itemC to the user
+myUser.addToCart(myShop.items[2]);
+myUser.addToCart(myShop.items[2]);
+myUser.addToCart(myShop.items[2]);
+myUser.addToCart(myShop.items[2]);
+myUser.printCart();
+console.log('=====================================================');
+// Remove all of itemB
+myUser.removeFromCart(myShop.items[1]);
+myUser.printCart();
+console.log('=====================================================');
+// Remove 2 of itemC
+myUser.removeQuantityFromCart(myShop.items[2], 2);
+myUser.printCart();
